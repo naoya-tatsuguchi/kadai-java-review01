c86828b81e7828643b098ac54f5e50cac26df3fe
naoya.tatsuguchi <tkn-rk.atg115@docomo.ne.jp>
2024-11-07 22:49:53 +0900

    Lesson4

---
+++ src/IfSample1.java
@@ -0,0 +1,17 @@
+
+public class IfSample1 {
+
+    public static void main(String[] args) {
+        int score = -60; // 成績
+        
+        //scoreが60以上ならば
+        if (score > 60) {
+            //合格ですと表示
+            System.out.println("合格です！");
+        }else{
+            System.out.println("赤点です...");
+        }
+
+    }
+
+}
