77770daac358cc341ca572e6cbaa2002b6ad269e
naoya.tatsuguchi <tkn-rk.atg115@docomo.ne.jp>
2024-11-07 17:45:25 +0900

    Lesson3課題｀

---
+++ src/Review01.java
@@ -0,0 +1,15 @@
+
+public class Review01 {
+
+    public static void main(String[] args) {
+        int num1 = 879;
+        int num2 = tax(num1) + num1;
+        System.out.println(num1 + "円の商品の税込価格は"+ num2 + "円(消費税は" + tax(num1) + "円)です。");
+    }
+    
+    public static int tax(int num1){
+        double result = (double) num1*0.23;
+        return (int) result;
+    }
+
+}
